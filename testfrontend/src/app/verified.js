d0637dccb14a76005a53dca0ac53da062630948f
AG <adityagautam.ag@gmail.com>
2025-01-19 02:35:10 -0500

    base webapp pages

---
+++ testfrontend/src/app/verified.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Verified = () => {
+  return (
+    <div>
+      <h1>Verified Page</h1>
+      <p>You have reached the verified page!</p>
+    </div>
+  );
+};
+
+export default Verified;
